f37eeb4137c374284bb7ba2b42c577053ad557fe
MichaelAlbright <michaelalbright25@yahoo.com>
2016-12-02 00:45:58 -0500

    632

--- Assets/__Scripts/BartokLayout.cs
+++ Assets/__Scripts/BartokLayout.cs
@@ -10,6 +10,13 @@ public class SlotDef
 	public bool faceUp=false;
 	public string layerName="Default";
 	public int layerID = 0;
+	public int id;
+	public List<int> hiddenBy = new List<int> ();
+	public float rot;
+	public string type = "slot";
+	public Vector2 stagger;
+	public int player;
+	public Vector3 pos;
 }
 
 public class BartokLayout : MonoBehaviour {
